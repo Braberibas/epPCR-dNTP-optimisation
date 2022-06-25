1929ed415630fdbea0327de0e4f27811ce2bdf8e
tbradl <bradleyt@ethz.ch>
2022-06-25 11:59:42 +0200

    Update .Rhistory, .gitignore, and 6 more files... Set up directory structure and shortened Rmd file from MSc thesis to contain only suitable content.

---
+++ App/app.R
@@ -0,0 +1,31 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+#### Load packages
+library(shiny)
+####----------------------------------------------------------------------####
+
+
+#### UI
+ui <- fluidPage(
+  
+)
+####----------------------------------------------------------------------####
+
+
+#### Server
+server <- function (input, output) {
+  # *Input() functions
+  # *Output() functions
+}
+####----------------------------------------------------------------------####
+
+
+#### App
+shinyApp(ui = ui, server = server)
